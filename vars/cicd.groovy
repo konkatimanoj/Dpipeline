bf210d3fa59f6bf22ac003d11035cb4a3aafe5ea
konkatimanoj <162788980+konkatimanoj@users.noreply.github.com>
2024-03-25 16:58:56 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,9 @@ def gitdeploy(jobname,ip,name)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${name}.war"
 }
+def gitexec()
+{
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
+
